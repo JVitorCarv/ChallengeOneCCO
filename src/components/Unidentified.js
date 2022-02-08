d89a7886944b9522d7e490a7328b0dacc92fca00
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-02-08 17:29:20 -0300

    Does not show additional description when none is provided

--- src/components/Unidentified.js
+++ src/components/Unidentified.js
@@ -3,12 +3,16 @@ import { TextField } from "@mui/material";
 function Unidentified({ natureForm }) {
   return (
     <div className="view-data">
-      <TextField
-        label="Descrição adicional"
-        value={natureForm.description}
-        disabled
-        multiline
-      />
+      <p>A natureza do problema não foi identificada pelo operador.</p>
+
+      {natureForm.description && (
+        <TextField
+          label="Descrição adicional"
+          value={natureForm.description}
+          disabled
+          multiline
+        />
+      )}
     </div>
   );
 }
