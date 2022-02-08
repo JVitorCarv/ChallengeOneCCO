2557ebd72ecabba60fa3f4d3e63a8d44f22b9c3f
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-02-08 17:04:36 -0300

    Debug unidentified problem not showing up in DashboardTable

--- src/components/DashboardTable.js
+++ src/components/DashboardTable.js
@@ -23,7 +23,7 @@ const app = initializeApp(firebaseConfig);
 const natureObj = {
   1: "Eletrico",
   2: "Mecânico",
-  undefined: "Não Identificado",
+  3: "Não Identificado",
 };
 
 // Get a reference to the database service
