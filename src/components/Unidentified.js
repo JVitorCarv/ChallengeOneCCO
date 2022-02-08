241b071d84e111f32372f989e0cb33ddd28a8721
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-02-08 16:53:05 -0300

    Adds display of additional description of unidentified problems

--- src/components/Unidentified.js
+++ src/components/Unidentified.js
@@ -1,4 +1,15 @@
+import { TextField } from "@mui/material";
+
 function Unidentified({ natureForm }) {
-  return <pre>{JSON.stringify(natureForm, null, 2)}</pre>;
+  return (
+    <div className="view-data">
+      <TextField
+        label="Descrição adicional"
+        value={natureForm.description}
+        disabled
+        multiline
+      />
+    </div>
+  );
 }
 export default Unidentified;
